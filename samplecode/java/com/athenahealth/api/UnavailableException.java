0af86639b55b49d596e06240bc0964072acea6c9
Christopher Schultz <chris@christopherschultz.net>
2017-06-25 14:47:22 -0400

    Introduce a new exception type that indicates that athenanet is unavailable.

---
+++ samplecode/java/com/athenahealth/api/UnavailableException.java
@@ -0,0 +1,30 @@
+package com.athenahealth.api;
+
+/**
+ * Exception type indicating that AthenaNET API is unavailable because the
+ * server has returned a 503 response.
+ */
+public class UnavailableException
+    extends AthenahealthException {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a new UnavailableException with the specified detail message.
+     *
+     * @param message An explanation for the error.
+     */
+    public UnavailableException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new UnavailableException with the specified detail message
+     * and root cause.
+     *
+     * @param message An explanation for the error.
+     * @param cause The root cause of the error.
+     */
+    public UnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
